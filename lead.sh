260553920b9c680c50d6245d959751b266dc7e92
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-08-30 18:54:01 +0300

    Set environments ready for mac or linux and update lead sh

--- lead.sh
+++ lead.sh
@@ -3,9 +3,20 @@ PATH="$PATH:$HOME/.composer/vendor/bin"
 fpath=(/usr/local/share/zsh-completions $fpath)
 ZSH_DIR="$HOME/.zsh"
 
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
+skipFiles=("." ".." "lead.sh" ".git" ".gitignore" "environments-mac" "environments-linux")
+
 if [ -d $ZSH_DIR ]; then
    for envFile in $ZSH_DIR/*; do
-        if [ $envFile = '.' ] || [ $envFile = '..' ]; then
+	containsElement $(basename $envFile) "${skipFiles[@]}"
+
+        if [ $? = 0 ]; then
            continue
         fi
 
