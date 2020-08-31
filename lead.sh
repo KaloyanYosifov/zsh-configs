4bf73a80f64c1f4539460fe769eedccb5613f713
kyosifov <kyosifov@2create.studio>
2020-08-31 08:05:28 +0300

    Remove non unique environments

--- lead.sh
+++ lead.sh
@@ -10,7 +10,7 @@ containsElement () {
   return 1
 }
 
-skipFiles=("." ".." "lead.sh" ".git" ".gitignore" "environments-mac" "environments-linux")
+skipFiles=("." ".." "lead.sh" ".git" ".gitignore" "environments-mac" "environments-linux" "scripts")
 
 if [ -d $ZSH_DIR ]; then
    for envFile in $ZSH_DIR/*; do
