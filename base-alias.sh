8cd9dc714dcabe0eb3d6103dd1b589506bbfb29f
kyosifov <kyosifov@2create.studio>
2021-04-20 17:44:37 +0300

    Update to only master omain

--- base-alias.sh
+++ base-alias.sh
@@ -2,7 +2,7 @@ alias ll="ls -laGh"
 
 #github
 alias gs="git status"
-alias delLGB="git branch --merged | egrep -v \"(^\*|master|dev)\" | xargs git branch -d"
+alias delLGB="git branch --merged | egrep -v \"(^\*|master|main)\" | xargs git branch -d"
 
 #Laravel
 alias art="php artisan"
